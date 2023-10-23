6a5d8efe01dc5da7a6a7b6db15dc8c6eb1ffd40a
Gabriel Pereira <gabrield.p.2011@gmail.com>
2023-10-23 18:52:26 -0300

    atualizacao projetos

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,6 @@ const header = document.querySelector('header')
 
 window.addEventListener('scroll', () => {
     const alturaPagina = window.scrollY
-    if (alturaPagina < 10) header.classList.remove('scroll')
+    if (alturaPagina < 5) header.classList.remove('scroll')
     else header.classList.add('scroll')
 })
